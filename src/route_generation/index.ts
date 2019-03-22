8ebcdd51e29023e59132d04b0ed72c50ae45109d
Robert Maloney <robertjamesmaloney@gmail.com>
2019-03-22 14:24:02 -0700

    Move route helpers into individual source files

--- src/route_generation/index.ts
+++ src/route_generation/index.ts
@@ -5,8 +5,29 @@ export default (path: string, requiredArguments: string[]) => {
         path = path.replace(`:${arg}`, `\${${arg}}`);
     });
 
+    let requiredArgDefinition =
+      (requiredArguments.length > 0)
+        ? '\ntype RequiredArg = string | number;\n'
+        : '';
+
     return (
-`import { generateFormatAndQuery, RequiredArg } from 'tarr';
+`import { stringify } from 'qs';
+${requiredArgDefinition}
+function generateFormatAndQuery(
+    formatOrQuery?: string | object, query?: object
+): { formatString: string, queryString: string } {
+  let queryString = '';
+  let formatString = '';
+  if (query) {
+    queryString = \`?\${stringify(query)}\`;
+    formatString = formatOrQuery ? \`.\${formatOrQuery}\` : '';
+  } else if (typeof formatOrQuery === 'string') {
+    formatString = formatOrQuery ? \`.\${formatOrQuery}\` : '';
+  } else if (typeof formatOrQuery === 'object' && formatOrQuery !== null) {
+    queryString = \`?\${stringify(formatOrQuery)}\`;
+  }
+  return { formatString, queryString };
+}
 
 function route(${paramString}formatOrQuery?: string | object, query?: object) {
   let { formatString, queryString } = generateFormatAndQuery(formatOrQuery, query);
