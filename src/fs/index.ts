3d69ebe40e77e71f1ac336541edbaca467685036
Robert Maloney <robertjamesmaloney@gmail.com>
2019-03-22 14:22:33 -0700

    Accept routes.json path as an argument

--- src/fs/index.ts
+++ src/fs/index.ts
@@ -1,22 +1,3 @@
-import * as fs from 'fs';
-import { join as pathJoin } from 'path';
+import RouteFileSystem from './route_file_system';
 
-export default class RouteFileSystem {
-  private root: string;
-
-  constructor(root: string) {
-    if (!fs.existsSync(root)) {
-      throw new Error(`Routes folder \`${root}\` does not exist`);
-    }
-    this.root = root;
-  }
-
-  addRouteScript(controllerName: string, routeName: string, routeSource: string) {
-    const controllerPath = pathJoin(this.root, controllerName);
-    if (!fs.existsSync(controllerPath)) {
-      fs.mkdirSync(controllerPath);
-    }
-
-    fs.writeFileSync(pathJoin(this.root, controllerName, `${routeName}.ts`), routeSource);
-  }
-}
+export default RouteFileSystem;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as yargs from 'yargs';
 import generateRouteScript from './route_generation';
 import RouteFileSystem from './fs';
+import * as fs from 'fs';
 
 const argv = yargs
   .usage('Usage: $0 -d [folder] -r [routes_json]')
@@ -12,12 +13,22 @@ const argv = yargs
 
 console.log(`Generating routes in folder ${argv.directory}`);
 
-const routesJSON = JSON.parse(argv.routes as string);
+const routesJSON = JSON.parse(fs.readFileSync(argv.routes as string).toString());
 const rfs = new RouteFileSystem(argv.directory as string);
 
 Object.keys(routesJSON).forEach((controllerKey) => {
   Object.keys(routesJSON[controllerKey]).forEach((routeKey) => {
-    const { path, req } = routesJSON[controllerKey][routeKey];
+    const route = routesJSON[controllerKey][routeKey];
+
+    let path, req;
+    if (typeof route === 'string') {
+      path = route;
+      req = [];
+    } else {
+      path = route.path;
+      req = route.req;
+    }
+
     const routeSource = generateRouteScript(path, req);
     rfs.addRouteScript(controllerKey, routeKey, routeSource);
   });
